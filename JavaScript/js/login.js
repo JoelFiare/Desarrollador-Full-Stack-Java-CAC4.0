c8b5d171fd4b300943f228820370640753928777
JoelFiare <JoelFiare@gmail.com>
2023-05-05 12:42:13 -0300

    anotaciones

--- JavaScript/js/login.js
+++ JavaScript/js/login.js
@@ -1,15 +1,21 @@
 const apuntadorALogin = document.getElementById("btnLogin");
+//asocio el evento click
 apuntadorALogin.addEventListener("click", Login);
 
 function Login() {
+  // capturo el username
   const username = document.getElementById("username").value;
+  // capturo el password
   const password = document.getElementById("password").value;
+  //scope de una variable
   const resultado = document.getElementById("resultado");
 
-  //validar que los dos datos username y password estén completos
+  //valido que se hayan completado los campos username y password
+  //guard
   if (username === "" || password === "") {
     resultado.innerHTML = "Complete todos los campos";
   } else {
+    //evaluó
     if (username === "admin" && password === "123") {
       resultado.innerHTML = "Login correcto";
     } else {
